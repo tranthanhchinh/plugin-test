bf2e9a0c3c3cd4078a4cd776387ec63937bdb02f
Thanh Chinh <tranthanhchinh.hd@gmail.com>
2022-09-14 01:09:38 +0700

    Edit v5

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: Plugin Test
 Plugin URI: https://chinhwp.com/
 Description: Viewer List Images Post
 Author: Thanh Chinh
-Version: 4.0.0
+Version: 5.0.0
 Author URI: https://chinhwp.com/
  */
 
