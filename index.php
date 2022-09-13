88c2c20aef7ba9af492471eab98b063b8dff9e19
Thanh Chinh <tranthanhchinh.hd@gmail.com>
2022-09-14 01:42:58 +0700

    Edit v6.1.1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: Plugin Test
 Plugin URI: https://chinhwp.com/
 Description: Viewer List Images Post
 Author: Thanh Chinh
-Version: 6.1.0
+Version: 6.1.1
 Author URI: https://chinhwp.com/
  */
 
@@ -14,7 +14,7 @@ Author URI: https://chinhwp.com/
     $updater->set_repository('plugin-test');
     $updater->authorize('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');
     $updater->initialize();
-    print_r($updater->initialize());
+
 
 add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'salcode_add_plugin_page_settings_link');
 function salcode_add_plugin_page_settings_link( $links ) {
